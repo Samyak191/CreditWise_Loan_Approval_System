e860975423652dcebd1de71d4f4826eeb8a90b2e
Samyak191 <1jainsamyak@gmail.com>
2026-03-20 16:34:25 +0530

    Updated App with Model Prediction

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import pickle
+
+# Load model
+model = pickle.load(open("model.pkl", "rb"))
 
 st.title("CreditWise Loan Approval System")
-st.write("App is live 🚀")
+
+st.write("Enter details to check loan approval")
+
+income = st.number_input("Income")
+loan_amount = st.number_input("Loan Amount")
+
+if st.button("Predict"):
+    prediction = model.predict([[income, loan_amount]])
+    
+    if prediction[0] == 1:
+        st.success("✅ Loan Approved")
+    else:
+        st.error("❌ Loan Rejected")
